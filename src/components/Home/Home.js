f222582d2af8dbfcd75ee91fb97a473f1b9be078
ssobii2 <71027266+ssobii2@users.noreply.github.com>
2022-05-17 19:47:23 +0200

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
                 <Box sx={{display : "flex", flexDirection : "column", marginLeft: "3rem"}}>
                     <Typography sx={{color: "#bd3b22"}} variant="h3">Hi! there <div className="rotate">👋</div> I am</Typography>
                     <Typography className="animation" sx={{fontFamily: "sans-serif", fontWeight: "600"}} variant="h2">SUBHAN IMRAN</Typography>
-                    <Typography sx={{color: "#2C394B", fontWeight: "400", paddingRight: "20px"}} variant="h2">I want to make awesome websites</Typography>
+                    <Typography sx={{color: "#2C394B", fontWeight: "400", paddingRight: "20px"}} variant="h3">I want to make awesome websites</Typography>
                     <Typography sx={{fontWeight: "normal"}} variant="h6">Want to become Full stack web developer. Based in PAKISTAN</Typography>
                     <Link href="#contact" underline="none">
                         <Button sx={{width: "20%", height: "3rem", marginTop: "10px"}} color="error" variant="outlined">Get in Touch</Button>
